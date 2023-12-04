500d8b62fdacf4ddbd15077661bf9c0c4134ed3a
Marah Habashi <118184558+MarahHab@users.noreply.github.com>
2023-12-04 09:54:27 +0200

    add delete post button

--- render.js
+++ render.js
@@ -8,13 +8,20 @@ function Renderer() {
 
       for (let comment of post.comments) {
         const commentHTML = `<div class="comment" id="${comment.id}"><span class="comment-text"><button class='delete-comment'>X</button>${comment.text}</span>`;
-        
+
         $(`#${post.id}`).append(commentHTML);
       }
-      const deletePostBtn = `<button class = "delete" > Delete Post</button>`;
-      const commentBtn = `<button class = "add-comment"> Comment</button>`;
+      const deletePostBtn = `<button class = "delete button-45" class="button-45" role="button"> Delete Post</button>`;
+      
+      const inputNewTxt = $(
+        "<input class='comment-input' placeholder='Write a new comment...'>"
+      );
+      const publishBtn = $(
+        "<button class='publish-comment-btn'>Publish</button>"
+      );
+      $(`#${post.id}`).append(inputNewTxt, publishBtn);
       $(`#${post.id}`).append(deletePostBtn);
-      $(`#${post.id}`).append(commentBtn)
+      
     }
   }
   return { renderPosts };
